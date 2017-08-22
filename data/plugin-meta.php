0c22d947252a36382baf9da31fc49d98734a944e
MM-DESKTOP-MAX\Maxim Strukov <max@miller-media.com>
2017-08-21 17:45:21 -0700

    Updating the version

--- data/plugin-meta.php
+++ data/plugin-meta.php
@@ -6,6 +6,6 @@ return <<<'JSON'
     "name": "Product Report",
     "namespace": "MillerMedia\\ProductReport",
     "slug": "millermedia-productreport",
-    "version": "1.0.3"
+    "version": "1.0.4"
 }
 JSON;
